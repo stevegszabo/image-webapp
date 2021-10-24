c8656fef1aeebb2318bcaf894751cef0732751db
Steve Szabo <steve.g.szabo@gmail.com>
2021-10-24 16:07:40 -0400

    updates

--- root/app/entrypoint.sh
+++ root/app/entrypoint.sh
@@ -2,7 +2,7 @@
 
 WEBAPP_BIND="0.0.0.0:8080"
 WEBAPP_APP="app.webapp:GAPPLICATION"
-WEBAPP_LEVEL="debug"
+WEBAPP_LEVEL="info"
 
 gunicorn3 --bind $WEBAPP_BIND $WEBAPP_APP --log-level $WEBAPP_LEVEL --access-logfile - --error-logfile -
 
