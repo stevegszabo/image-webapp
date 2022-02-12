2091fc2d5ab797375a5e225730463702b4a3977e
stevegszabo <steve.g.szabo@gmail.com>
2022-02-12 12:08:18 -0500

    updates

--- root/app/entrypoint.sh
+++ root/app/entrypoint.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 WEBAPP_BIND="0.0.0.0:8080"
 WEBAPP_APP="app.webapp:GAPPLICATION"
-WEBAPP_LEVEL="debug"
+WEBAPP_LEVEL="info"
 
 gunicorn3 --bind $WEBAPP_BIND $WEBAPP_APP --log-level $WEBAPP_LEVEL --access-logfile - --error-logfile -
 
