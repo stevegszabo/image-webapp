ea1accc42f37786144551928032db261afe6da6f
Steve Szabo <steve.g.szabo@gmail.com>
2022-02-26 09:07:47 -0500

    updates

--- root/app/entrypoint.sh
+++ root/app/entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -o errexit
 
+PROFILE=/vault/secrets/config
+
+if [ -f $PROFILE ]; then
+    source $PROFILE
+else
+    POSTGRES_USER=user
+    POSTGRES_PASSWORD=pass
+    export POSTGRES_USER POSTGRES_PASSWORD
+fi
+
 WEBAPP_BIND="0.0.0.0:8080"
 WEBAPP_APP="app.webapp:GAPPLICATION"
 WEBAPP_LEVEL="info"
