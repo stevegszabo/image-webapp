37e660dcef2f62f7ee490c9a0848aa4007f02a90
stevegszabo <steve.g.szabo@gmail.com>
2022-03-02 18:57:11 -0500

    updates

--- root/app/entrypoint.sh
+++ root/app/entrypoint.sh
@@ -2,22 +2,15 @@
 
 set -o errexit
 
-PROFILE=/vault/secrets/config
-
-if [ -f $PROFILE ]; then
-    source $PROFILE
-else
-    POSTGRES_USER=user
-    POSTGRES_PASSWORD=pass
-    export POSTGRES_USER POSTGRES_PASSWORD
-fi
-
+WEBAPP_PROFILE=${WEBAPP_PROFILE-/vault/secrets/config}
 WEBAPP_VERSION=${WEBAPP_VERSION-0000000}
 WEBAPP_ADDRESS=${WEBAPP_ADDRESS-0.0.0.0}
 WEBAPP_PORT=${WEBAPP_PORT-8080}
 WEBAPP_LOG_LEVEL=${WEBAPP_LOG_LEVEL-debug}
 WEBAPP_APPLICATION=${WEBAPP_APPLICATION-app.webapp:GAPPLICATION}
 
+source "$WEBAPP_PROFILE" || exit 1
+
 gunicorn3 --bind "$WEBAPP_ADDRESS:$WEBAPP_PORT" "$WEBAPP_APPLICATION" --log-level "$WEBAPP_LOG_LEVEL" --access-logfile - --error-logfile -
 
-exit 0
+exit $?
