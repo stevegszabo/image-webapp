79ac1608cb63accd6b633005e545d95333b59b18
stevegszabo <steve.g.szabo@gmail.com>
2024-01-14 08:35:39 -0500

    updates

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+GOOGLE_ENVIRONMENT=engineering
+GOOGLE_PROJECT=$GOOGLE_ENVIRONMENT-406619
+GOOGLE_INSTANCE=$GOOGLE_ENVIRONMENT-01
+GOOGLE_REGISTRY=us-east1-docker.pkg.dev/$GOOGLE_PROJECT/$GOOGLE_INSTANCE
+GOOGLE_IMAGE_TAG=0000001
+
+docker build \
+--build-arg WEBAPP_VERSION=$GOOGLE_IMAGE_TAG \
+--build-arg WEBAPP_ADDRESS=0.0.0.0 \
+--build-arg WEBAPP_PORT=8080 \
+--build-arg WEBAPP_LOG_LEVEL=info \
+--build-arg WEBAPP_APPLICATION=app.webapp:GAPPLICATION \
+--build-arg WEBAPP_PROFILE=/vault/secrets/config \
+--build-arg WEBAPP_DATABASE=database \
+-t $GOOGLE_REGISTRY/webapp:$GOOGLE_IMAGE_TAG .
+
+exit 0
