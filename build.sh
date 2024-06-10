f04115f1d0181ae4abb51b970679524e3d2d274f
stevegszabo <steve.g.szabo@gmail.com>
2024-06-10 19:48:25 -0400

    updates

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o pipefail
 
-GOOGLE_REGISTRY=us-east1-docker.pkg.dev/engineering-line-01-qum4gi/gar-engineering-01
+GOOGLE_REGISTRY=us-east1-docker.pkg.dev/eng-lob-01-l2gp4k/gar-01
 GOOGLE_IMAGE_REPO=${1-webapp}
 GOOGLE_IMAGE_TAG=${2-v1.0.1}
 GOOGLE_IMAGE=$GOOGLE_REGISTRY/$GOOGLE_IMAGE_REPO:$GOOGLE_IMAGE_TAG
