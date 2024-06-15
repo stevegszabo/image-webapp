1607b67b4211f41139173ee2785c704be92ec970
Steve Szabo <steve.g.szabo@gmail.com>
2024-06-15 10:38:15 -0400

    updates

--- root/app/webapp.py
+++ root/app/webapp.py
@@ -8,10 +8,10 @@ from flask import Flask
 
 GAPPLICATION = Flask(__name__)
 GVERSION = os.getenv('WEBAPP_VERSION', '0000000')
-GDBINSTANCE = os.getenv('WEBAPP_DATABASE', 'none')
 GADDRESS = os.getenv('WEBAPP_ADDRESS', '0.0.0.0')
 GPORT = os.getenv('WEBAPP_PORT', 8080)
 
+GDBINSTANCE = os.getenv('WEBAPP_DATABASE', 'none')
 GDBUSER = os.getenv('POSTGRES_USER', 'POSTGRES_USER')
 GDBPASSWORD = os.getenv('POSTGRES_PASSWORD', 'POSTGRES_PASSWORD')
 
